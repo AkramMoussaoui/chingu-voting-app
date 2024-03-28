e1009091528abb56d5ce77b5536adcfcda588bb9
AkramMoussaoui <akrammoussaoui14@gmail.com>
2024-03-28 19:06:54 +0100

    Add items dynamically

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,25 @@
-// const itemContainer = document.querySelector('.item_container');
+const itemContainer = document.querySelector(".item_container");
 
-axios.get("https://my.api.mockaroo.com/voting.json?key=fd46f130").then(response => {
-    console.log(response.data)
-})
+const getData = async () => {
+  const { data } = await axios.get(
+    "https://my.api.mockaroo.com/voting.json?key=fd46f130"
+  );
+  const totalVotes = data.reduce((acc, value) => acc + value.Score, 0);
+  console.log(totalVotes);
+  const voteItems = data.map(
+    (item) => `
+        <span class="subject">${item.Choice}</span>
+        <span class="score">${((item.Score / totalVotes) * 100).toFixed(
+          2
+        )}%</span>
+    `
+  );
+  voteItems.forEach((element) => {
+    const div = document.createElement("div");
+    div.classList.add("voting_item");
+    div.innerHTML = element;
+    itemContainer.appendChild(div);
+  });
+};
 
-// const getData = async () => {
-//     const response = await axios.get("https://my.api.mockaroo.com/voting.json?key=fd46f130")
-//     console.log(response.data)
-// }
-
-// getData()
-
+getData();
