84643cd898466aadde298c90535b9397eeee903f
devi-lakshmi <devi.venki8@gmail.com>
2024-03-28 21:22:30 +0100

    mapped with Api

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,45 @@
-// const itemContainer = document.querySelector('.item_container');
 
-axios.get("https://my.api.mockaroo.com/voting.json?key=fd46f130").then(response => {
-    console.log(response.data)
-})
+const itemContainer = document.querySelector('.item_container');
 
-// const getData = async () => {
-//     const response = await axios.get("https://my.api.mockaroo.com/voting.json?key=fd46f130")
+// axios.get("https://my.api.mockaroo.com/voting.json?key=fd46f130").then(response => {
 //     console.log(response.data)
-// }
+// })
+const getData = async () => {
+    const { data } = await axios.get('https://my.api.mockaroo.com/voting.json?key=fd46f130')
+    const voteItems = data.map(item => `
+    <span class = "subject"> ${item.Choice}</span>
+    <span class = "score"> ${item.Score}</span>`)
 
-// getData()
+    voteItems.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML = element;
+        itemContainer.appendChild(div);
+
+    });
+}
+getData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
